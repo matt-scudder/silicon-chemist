ff53fa478d84e6cb08b1405134ea638b6f76b2a7
Vinushika <Vinushika>
2016-01-08 13:14:12 -0500

    Server now has a shiny test page!

---
+++ sic/sigc/server.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+#coding=utf-8
+"""
+This is the server for SiGC. It should contain a minimum of logic
+not related to routes for the website, and offload as much
+processing as possible to the other modules.
+"""
+import traceback #never know when you'll need this.
+from flask import Flask
+from flask import render_template
+from .. import sic #where the magic happens - note the relative import!
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def jsme_display():
+	"""
+	Displays the JSME interface for creating reactant and product sets.
+	"""
+	return render_template("reactions/reaction_sketch.html") #add in template vars as needed...
+
+@app.route("/submit_reaction")
+def accept_reaction():
+	"""
+	Handles reactant and product input from users and runs SiC³ on them.
+	"""
+	return "a response"
---
+++ sic/sigc/static/js/local/reactions.js
@@ -0,0 +1,38 @@
+/*
+   Takes care of all the stuff related to the test page.
+*/
+var reactants = [];
+var products = [];
+var solvent = []; //currently unused
+var jsmeApplet = null;
+
+function add_molecule(smiles,mol_type) {
+	/*
+	   Adds the reactant to the list of reactants, products or solvent (depending on mol_type), 
+	   and displays the reactant below the correct column.
+	   Right now this will just display SMILES strings. In the future we can use OpenBabel to display images (http://ctr.wikia.com/wiki/Depict_a_compound_as_an_image ) by querying the server with an AJAX request.
+	   */
+	switch(mol_type){
+		case "reactant":
+			reactants.push(smiles);
+			$("#add_reactant").before("<p>"+smiles+"</p>");
+			break;
+		case "product":
+			products.push(smiles)
+			$("#add_product").before("<p>"+smiles+"</p>");
+			break;
+		case "solvent":
+			solvent.push(smiles);
+			break;
+		default:
+			console.error("Bad mol_type passed in to add_molecule: " + mol_type + " for SMILES: " + smiles);
+			break;
+	}
+}
+
+function jsmeOnLoad() {
+	jsmeApplet = new JSApplet.JSME("jsme_container","380px","340px"); //figure out better values for the width/height
+	$("#add_reactant").click(function() { add_molecule(jsmeApplet.smiles(),"reactant");});
+	$("#add_product").click(function() { add_molecule(jsmeApplet.smiles(),"product");});
+	//$("#add_solvent").click(function() { add_molecule(jsmeApplet.smiles(),"solvent");});
+}
