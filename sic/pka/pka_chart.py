1c2a79b6216cc92a55bbd04c232119441e25dbde
Vinushika <Vinushika>
2016-01-11 12:38:46 -0500

    Well, it turns out that matching hydrogens is important.

--- sic/pka/pka_chart.py
+++ sic/pka/pka_chart.py
@@ -10,17 +10,19 @@ whether due to physical impossibility (pentavalent carbon)
 or it not being a number we have, we put None for that slot.
 The rest of the program should read these accordingly.
 """
+#TODO: Modify this if it turns out that non-sequential (i.e. as keys in a dict rather than a list) access is better, because the current access method sucks
+#NOTE: The huge amount of ([H]) below are because otherwise the hydrogens we so desperately care about won't get matched in the expression, and we run into problems.
 PKA_CHART = [
     #Water
-    {"[OH2X2]":{"pKa_HA":15.7,"pKa_BH":-1.7}}, #X2 required because it would match e.g. protonated carboxylics otherwise, which have diff. pKa
-    {"[OH3]":{"pKa_HA":-1.7,"pKa_BH":None}}, #can't add another H to OH3+, so None for BH
+    {"[OH2X2]([H])([H])":{"pKa_HA":15.7,"pKa_BH":-1.7}}, #X2 required because it would match e.g. protonated carboxylics otherwise, which have diff. pKa
+    {"[OH3]([H])([H])([H])":{"pKa_HA":-1.7,"pKa_BH":None}}, #can't add another H to OH3+, so None for BH
     #Halogens - no pKa_BH for the acids, pretty sure stuff like H2I doesn't exist.
-    {"[IH1]":{"pKa_HA":-10,"pKa_BH":None}},
+    {"[IH1]([H])":{"pKa_HA":-10,"pKa_BH":None}},
     {"[I!H1]":{"pKa_HA":None,"pKa_BH":-10}}, #Since other stuff that makes halogens better Ls doesn't go into the numeric pKa estimate, just make all Is -10
-    {"[BrH1]":{"pKa_HA":-9,"pKa_BH":None}},
+    {"[BrH1]([H])":{"pKa_HA":-9,"pKa_BH":None}},
     {"[Br!H1]":{"pKa_HA":None,"pKa_BH":-9}},#!H1 because we want halogens that AREN'T attached to H (and thus acidic...)
-    {"[ClH1]":{"pKa_HA":-7,"pKa_BH":None}},
+    {"[ClH1]([H])":{"pKa_HA":-7,"pKa_BH":None}},
     {"[Cl!H1]":{"pKa_HA":None,"pKa_BH":-7}},
-    {"[FH1]":{"pKa_HA":3.2,"pKa_BH":None}},
+    {"[FH1]([H])":{"pKa_HA":3.2,"pKa_BH":None}},
     {"[F!H1]":{"pKa_HA":None,"pKa_BH":3.2}}
 ]
