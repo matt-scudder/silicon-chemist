3c4498913db73d9cd6fe43e505fd187e7acc7a71
Vinushika <Vinushika>
2016-01-13 14:10:47 -0500

    Added interactions map so that we can easily figure out what kinds of reactions arise from a source/sink pair.

---
+++ sic/reaction_types/interactions.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+#coding=utf-8
+"""
+Lists the table of source-sink interactions, i.e. which mechanisms a particular pair can perform.
+By convention, we map sources to sinks, though it can be done either way.
+We assume that all sources can map to all sinks, and vice versa.
+"""
+
+INTERACTIONS = {
+        ("Y:","H-L"):"proton_transfer"
+        }
