5e73262ef9f25b72e88b02362259c011bd27de7f
Vinushika <Vinushika>
2016-01-13 14:10:25 -0500

    Added first draft of unit test for bond-breaking and making.

---
+++ sic/structure/tests/test_bonds.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+#coding=utf-8
+"""
+Tests whether the make_bond and break_bond methods work.
+Because these methods are crucial to our modification of the reaction state,
+it is very important that they work correctly.
+"""
