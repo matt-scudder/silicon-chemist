1b28821c02bca6aba4074603fe087a1e1cc14983
Vinushika <Vinushika>
2016-01-15 13:20:45 -0500

    Added tests for similarity coefficient and made fixes based on test.

--- sic/structure/similarity.py
+++ sic/structure/similarity.py
@@ -6,11 +6,14 @@ Handles all operations that have to do with structure similarity.
 
 def tanimoto(mol1,mol2):
     """
-    Returns the tanimoto coefficient between two molecules, using the 7-atom fingerprint (FP2).
+    Returns the tanimoto coefficient between two molecules, using the FP3 fingerprint.
+    Because FP2 ignores single-atom C,N,O fragments, and these are often of crucial importance
+    to determining whether we're at product or not, FP2 is not used. Other fingerprints may be
+    used if FP3 is insufficient.
     
     If the coefficient is 1, then the two molecules are definitely the same.
     """
-    return mol1.calcfp() | mol2.calcfp()
+    return mol1.calcfp(fptype="fp3") | mol2.calcfp(fptype="fp3")
 
 def is_same_state(state1,state2):
     """
@@ -27,9 +30,13 @@ def is_same_state(state1,state2):
         remove = False
         for st2_mol in copy_state2:
             if tanimoto(st1_mol,st2_mol) == 1:
+                print "st2 removed"
                 remove = st2_mol
+                break
         if remove:
             copy_state2.remove(st2_mol)
+
     if len(copy_state2) < 1:
         return True
+    return False
 
