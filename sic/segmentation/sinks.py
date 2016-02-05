af9b8501f49c21e67e1f38244436ec9f568e5bf5
Vinushika <Vinushika>
2016-02-05 09:59:28 -0500

    Added C-L definition.

--- sic/segmentation/sinks.py
+++ sic/segmentation/sinks.py
@@ -10,8 +10,9 @@ to the "letters" in the source name) should be left in the segmentation module.
 #TODO: Figure out how to exclude pKa 40+ stuff from consideration; maybe prune after the source/sink list is built?
 #TODO: Add carbon acids to the H-L sink list
 SINKS = {
-		"H-L":"[O!v1,S,N,F,Cl,Br,I]([H])",
-	#anything we know can bond to hydrogen, bonded with it
-	#NOTE: This ignores stuff like NaH (because H is "alone" and not bonded)
-                "C+":"[C+]" #carbocations - only take in the carbon!
-	}
+        "H-L":"[O!v1,S,N,F,Cl,Br,I]([H])",
+    #anything we know can bond to hydrogen, bonded with it
+    #NOTE: This ignores stuff like NaH (because H is "alone" and not bonded)
+        "C+":"[C+]", #carbocations - only take in the carbon!
+        "C-L":"[CX4]([!H,!C])" #C-L is an sp3 carbon (i.e. 4 connections), and the L is not a C or an H.
+    }
