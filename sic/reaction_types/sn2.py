49ea82424e72e0a219cba1520c601fe80e470ef7
Vinushika <Vinushika>
2016-02-05 13:30:17 -0500

    Added first draft of SN2 cross check and rearrangement.

---
+++ sic/reaction_types/sn2.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python
+#coding=utf-8
+"""
+Carries out an SN2 reaction.
+"""
+from reaction_type import ReactionType
+import structure.struct_ops as struct_ops
+import pka.pka as pka
+
+class SN2(ReactionType):
+    """
+    ReactionType object that carries out an SN2 reaction.
+    """
+    reaction_type = "SN2"
+
+    def cross_check(self):
+        """
+        The cross-check score is two-parted.
+        One part is the ΔpKa rule: the pKaBH of the leaving group atom
+        should be less than 10 units more basic than the incoming nucleophile, or:
+        pKaBH(Nu) - pKaBH(L) < -10
+        The second part is ranking by softness of the Nu, which is not yet implemented.
+        """
+        if self.cross_check_score != -2.0:
+            return self.cross_check_score
+        nucleophile = self.sources[0]
+        sink = self.sinks[0]
+        #evaluation of the pKaBH of the Nu is easy - just get it off the atom
+        #TODO: Update this code so that it supports more than just Y
+        pka_BHNu = pka.get_pka(source["atoms"]["Y"]["atom"],source["atoms"]["Y"]["molecule"])
+        #for the L it's harder because the pKa changes based on whether it's bonded to the C
+        #we need to break the bond, see how that affects the pKa, and then use that number
+        #for now, we copy the molecule, break the bond there, and recalc pKa
+        new_mol = struct_ops.copy_molecule(source["atoms"]["Y"]["molecule"]) #the molecule passed in is arbitrary - remember it's the same for source and sink
+        new_mol.break_bond(sink["atoms"]["C"],sink["atoms"]["L"]) #this works because we use indices, not direct atom.OBAtom refs
+        new_mol.get_all_pka()
+        pka_BHL = pka.get_pka(sink["atoms"]["L"],new_mol)
+        #hope the garbage collector kills new_mol here and move on
+        dpKa = pKa_BHNu - pKa_BHL
+        if dpKa < -10: 
+            self.cross_check_score = 0.0
+            return self.cross_check_score
+        elif dpKa <= 0:
+            self.cross_check_score = 0.6 - (0.6*abs(dpKa/ 10.0))
+            return self.cross_check_score
+        elif dpKa > 10:
+            self.cross_check_score = 1.0
+            return self.cross_check_score
+        else:
+            self.cross_check_score = 0.6 + (0.4*abs(dpKa/ 10.0))
+            return self.cross_check_score
+
+        def rearrange(self):
+            """
+            Breaks the C-L bond and makes the C-Nu bond.
+            Stereochem is not taken into account here.
+            """
+            source = self.sources[0]
+            sink = self.sinks[0]
+            struct_ops.make_bond(source["atoms"]["Y"],sink["atoms"]["C"])
+            struct_ops.break_bond(sink["atoms"]["C"],sink["atoms"]["L"])
