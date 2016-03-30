ea6e4143d2fc54d559b46c7e2c0dd40fb81812af
Vinushika <Vinushika>
2016-03-30 19:34:56 -0400

    Added nitriles and deprotonated alcohols to pka_chart.

--- sic/pka/pka_chart.py
+++ sic/pka/pka_chart.py
@@ -10,8 +10,12 @@ whether due to physical impossibility (pentavalent carbon)
 or it not being a number we have, we put None for that slot.
 The rest of the program should read these accordingly.
 """
+#NOTE: This file will get REALLY big really fast, but only needs to be run once per ReactionState
 #NOTE: The order here is of paramount importance such that species are matched correctly. Start with the least specific forms, and then go to the most specific.
 #NOTE: The huge amount of ([H]) below are because otherwise the hydrogens we so desperately care about won't get matched in the expression, and we run into problems.
+#NOTE: There is no expression that catches "any number of hydrogens" as far as my current (3/30/16) research indicates, since SMARTS is all about 
+#       backbone stuff, and not so much about matching hydrogens. Will see if
+#       there's a better way eventually.
 PKA_CHART = [
     #Water
     {"[OHv1]([H])":{"pKa_HA": 52, "pKa_BH": 15.7}}, #v1 because otherwise every alcohol ever would match it. Put in a separate case for alcohols...
@@ -27,14 +31,29 @@ PKA_CHART = [
     {"[FH1]([H])":{"pKa_HA":3.2,"pKa_BH":None}},
     {"[F!H1]":{"pKa_HA":None,"pKa_BH":3.2}},
     #Alcohols
+    #protonated
     {"[CH3][O]([H])":{"pKa_HA": 15.5, "pKa_BH": -2.4}}, #methyl
     {"[CH2][O]([H])":{"pKa_HA": 16, "pKa_BH": -2.4}}, #primary
     {"[CH1][O]([H])":{"pKa_HA": 18, "pKa_BH": -3.5}}, #secondary
     {"[CH0][O]([H])":{"pKa_HA": 19, "pKa_BH": -4}}, #tertiary
+    #deprotonated
+    {"[CH3][O-]":{"pKa_HA": None, "pKa_BH": 15.5}}, #methyl
+    {"[CH2][O-]":{"pKa_HA": None, "pKa_BH": 16}}, #primary
+    {"[CH1][O-]":{"pKa_HA": None, "pKa_BH": 18}}, #secondary
+    {"[CH0][O-]":{"pKa_HA": None, "pKa_BH": 19}}, #tertiary
     #Protonated alcohols
     {"[CH3][OH2]([H])([H])":{"pKa_HA": -2.4, "pKa_BH": None}}, #methyl
     {"[CH2][OH2]([H])([H])":{"pKa_HA": -2.4, "pKa_BH": None}}, #primary
     {"[CH1][OH2]([H])([H])":{"pKa_HA": -3.5, "pKa_BH": None}}, #secondary
-    {"[CH0][OH2]([H])([H])":{"pKa_HA": -4, "pKa_BH": None}} #tertiary
+    {"[CH0][OH2]([H])([H])":{"pKa_HA": -4, "pKa_BH": None}}, #tertiary
+    #Nitriles (carbon acid)
+    {"[NX1]#[CX2]([H])":{"pKa_HA": 9.2, "pKa_BH": None}}, #cyanide, with proton
+    {"[NX1]#[#6-]":{"pKa_HA": None, "pKa_BH": 9.2}}, #cyanide, deprotonated
+    {"[NX1]#[CX2][CH3]([H])([H])([H])":{"pKa_HA":25,"pKa_BH":None}}, #nitrile CH, protonated, primary
+    {"[NX1]#[CX2][#6-]([H])([H])":{"pKa_HA":None,"pKa_BH":25}}, #nitrile CH, deprotonated, primary
+    {"[NX1]#[CX2][CH3]([H])([H])":{"pKa_HA":25,"pKa_BH":None}}, #nitrile CH, protonated, secondary 
+    {"[NX1]#[CX2][#6-]([H])":{"pKa_HA":None,"pKa_BH":25}}, #nitrile CH, deprotonated, secondary
+    {"[NX1]#[CX2][CH3]([H])":{"pKa_HA":25,"pKa_BH":None}}, #nitrile CH, protonated, tertiary
+    {"[NX1]#[CX2][#6-]":{"pKa_HA":None,"pKa_BH":25}}, #nitrile CH, deprotonated, tertiary
 
 ]
