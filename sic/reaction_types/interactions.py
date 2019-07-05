fb83a524430a0aa489c4f54380ef21dc2730a3a6
Maria Shehata <mariashehata@Marias-MacBook-Air.local>
2019-07-05 15:35:52 -0400

    Adding new interactions fo addition

--- sic/reaction_types/interactions.py
+++ sic/reaction_types/interactions.py
@@ -11,5 +11,17 @@ INTERACTIONS = {
         ("Y","C-L"):["SN2","E2"], #substitution vs. elimination!
         ("Y","C+"):["AN","E1"],
         ("DUM","C-L"):["DN"], #because DN is basically "the sink interacting with itself and falling apart", we want one DUM for every C-L
-        ("C-","C-L"):["EB"]
+        ("C-","C-L"):["EB"],
+        ("C=C", "H-L"):["AE","ADE3"], 
+        ("C=C", "Y-L"):["AE", "NuL"],
+        ("Z=C", "H-L"):["proton_transfer","ADE3"],
+        # This is one square on page 248 of the book "Electron Flow of Organic Chemistry", but we have two lines because SMARTS doesn't have "OR" 
+        ("Y","ewg(O)-C=C"):["ADN"], 
+        ("Y","ewg(N)-C=C"):["ADN"],
+        # This is one square on page 248 of the book "Electron Flow of Organic Chemistry", but we have two lines because SMARTS doesn't have "OR" 
+        ("C-","ewg(O)-C=C"):["ADN"],
+        ("C-","ewg(N)-C=C"):["ADN"],
+
+        ("Y","Z=C"):["ADN"],
+        ("C-","Z=C"):["ADN"]
         }
